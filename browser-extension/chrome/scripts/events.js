39b9be1fd2613e96f9138e5bc6b928b26a86a65e
CorgiDev <corgidev@github.com>
2025-03-12 17:14:12 -0400

    Updated manifest and debating removing one of my script files.

--- browser-extension/chrome/scripts/events.js
+++ browser-extension/chrome/scripts/events.js
@@ -2,67 +2,69 @@
 ELEMENT VARIABLES 
 *******************/
 /* Skills - Stunts */
-const stuntsHeading = document.getElementById('stunts-heading');
-const stuntsDie = document.getElementById('stunts-die');
-const stuntsCode = document.getElementById('stunts-code');
-const stuntsRollHereButton = document.getElementById('stunts-RollHere');
-const stuntsResult = document.getElementById('stunts-result');
+    const stuntsHeading = document.getElementById('stunts-heading');
+    const stuntsDie = document.getElementById('stunts-die');
+        let stuntsDieValue = stuntsDie.value;
+    const stuntsCode = document.getElementById('stunts-code');
+    const stuntsRollCode = document.getElementById('stunts-RollCode');
+    const stuntsRollHereButton = document.getElementById('stunts-RollHere');
+    const stuntsResult = document.getElementById('stunts-result');
 /* Skills - Brawl */
-const brawlHeading = document.getElementById('brawl-heading');
-const brawlDie = document.getElementById('brawl-die');
-const brawlCode = document.getElementById('brawl-code');
-const brawlRollHereButton = document.getElementById('brawl-RollHere');
-const brawlResult = document.getElementById('brawl-result');
+    const brawlDie = document.getElementById('brawl-die');
+    const brawlCode = document.getElementById('brawl-code');
+    const brawlRollCode = document.getElementById('brawl-RollCode');
+    const brawlRollHereButton = document.getElementById('brawl-RollHere');
+    const brawlResult = document.getElementById('brawl-result');
 /* Skills - Tough */
-const toughHeading = document.getElementById('tough-heading');
-const toughDie = document.getElementById('tough-die');
-const toughCode = document.getElementById('tough-code');
-const toughRollHereButton = document.getElementById('tough-RollHere');
-const toughResult = document.getElementById('tough-result');
+    const toughDie = document.getElementById('tough-die');
+    const toughCode = document.getElementById('tough-code');
+    const toughRollCode = document.getElementById('tough-RollCode');
+    const toughRollHereButton = document.getElementById('tough-RollHere');
+    const toughResult = document.getElementById('tough-result');
 /* Skills - Tech */
-const techHeading = document.getElementById('tech-heading');
-const techDie = document.getElementById('tech-die');
-const techCode = document.getElementById('tech-code');
-const techRollHereButton = document.getElementById('tech-RollHere');
-const techResult = document.getElementById('tech-result');
+    const techDie = document.getElementById('tech-die');
+    const techCode = document.getElementById('tech-code');
+    const techRollCode = document.getElementById('tech-RollCode');
+    const techRollHereButton = document.getElementById('tech-RollHere');
+    const techResult = document.getElementById('tech-result');
 /* Skills - Weapons */
-const weaponsHeading = document.getElementById('weapons-heading');
-const weaponsDie = document.getElementById('weapons-die');
-const weaponsCode = document.getElementById('weapons-code');
-const weaponsRollHereButton = document.getElementById('weapons-RollHere');
-const weaponsResult = document.getElementById('weapons-result');
+    const weaponsDie = document.getElementById('weapons-die');
+    const weaponsCode = document.getElementById('weapons-code');
+    const weaponsRollCode = document.getElementById('weapons-RollCode');
+    const weaponsRollHereButton = document.getElementById('weapons-RollHere');
+    const weaponsResult = document.getElementById('weapons-result');
 /* Skills - Drive */
-const driveHeading = document.getElementById('drive-heading');
-const driveDie = document.getElementById('drive-die');
-const driveCode = document.getElementById('drive-code');
-const driveRollHereButton = document.getElementById('drive-RollHere');
-const driveResult = document.getElementById('drive-result');
+    const driveDie = document.getElementById('drive-die');
+    const driveCode = document.getElementById('drive-code');
+    const driveRollCode = document.getElementById('drive-RollCode');
+    const driveRollHereButton = document.getElementById('drive-RollHere');
+    const driveResult = document.getElementById('drive-result');
 /* Skills - Sneak */
-const sneakHeading = document.getElementById('sneak-heading');
-const sneakDie = document.getElementById('sneak-die');
-const sneakCode = document.getElementById('sneak-code');
-const sneakRollHereButton = document.getElementById('sneak-RollHere');
-const sneakResult = document.getElementById('sneak-result');
+    const sneakDie = document.getElementById('sneak-die');
+    const sneakCode = document.getElementById('sneak-code');
+    const sneakRollCode = document.getElementById('sneak-RollCode');
+    const sneakRollHereButton = document.getElementById('sneak-RollHere');
+    const sneakResult = document.getElementById('sneak-result');
 /* Skills - Wits */
-const witsHeading = document.getElementById('wits-heading');
-const witsDie = document.getElementById('wits-die');
-const witsCode = document.getElementById('wits-code');
-const witsRollHereButton = document.getElementById('wits-RollHere');
-const witsResult = document.getElementById('wits-result');
+    const witsDie = document.getElementById('wits-die');
+    const witsCode = document.getElementById('wits-code');
+    const witsRollCode = document.getElementById('wits-RollCode');
+    const witsRollHereButton = document.getElementById('wits-RollHere');
+    const witsResult = document.getElementById('wits-result');
 /* Skills - Hot */
-const hotHeading = document.getElementById('hot-heading');
-const hotDie = document.getElementById('hot-die');
-const hotCode = document.getElementById('hot-code');
-const hotRollHereButton = document.getElementById('hot-RollHere');
-const hotResult = document.getElementById('hot-result');
+    const hotDie = document.getElementById('hot-die');
+    const hotCode = document.getElementById('hot-code');
+    const hotRollCode = document.getElementById('hot-RollCode');
+    const hotRollHereButton = document.getElementById('hot-RollHere');
+    const hotResult = document.getElementById('hot-result');
 
 /****************
 EVENT LISTENERS
 ****************/
 /* Skills - Stunts */
-stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDie.value, stuntsCode.id));
-myButton.addEventListener('click', handleClick.bind(null, 'Hello from bind!'));
-myButton.addEventListener('click', handleClick.bind(null, 'Hello from bind!'));
+stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id));
 /* Skills - Brawl */
 /* Skills - Tough */
 /* Skills - Tech */
@@ -71,5 +73,3 @@ myButton.addEventListener('click', handleClick.bind(null, 'Hello from bind!'));
 /* Skills - Sneak */
 /* Skills - Wits */
 /* Skills - Hot */
-
-/* Passing variables to event listeners */
--- browser-extension/chrome/scripts/nsbu.js
+++ browser-extension/chrome/scripts/nsbu.js
@@ -6,12 +6,12 @@ function RollDice(sides) {
 
 // Updates the Roll20 Text in the code block
 function UpdateCopyText(skillName,skillValue,skillCodeDiv) {
-    const skilldiv = document.getElementById(skillCodeDiv)
+    const skillDiv = document.getElementById(skillCodeDiv)
     if (skillValue ==0){
-        skilldiv.textContent = "Skill currently does not have a valid value set.";
+        skillCodeDiv.textContent = "Skill currently does not have a valid value set.";
     } else {
         let skillText="/me Rolled a " + skillName + " roll (1d"+skillValue + ") [[1d" + skillValue + "]]";
-        skilldiv.textContent = skillText;
+        skillCodeDiv.textContent = skillText;
     }
 }
 
@@ -67,3 +67,102 @@ function CopyToClipboard(containerid) {
     // Alert the copied text
     alert("Copied the text: " + copyText.value);
 } */
+
+/*******************
+ELEMENT VARIABLES 
+*******************/
+/* Skills - Stunts */
+    const stuntsHeading = document.getElementById('stunts-heading');
+    const stuntsDie = document.getElementById('stunts-die');
+    const stuntsCode = document.getElementById('stunts-code');
+    const stuntsRollCode = document.getElementById('stunts-RollCode');
+    const stuntsRollHereButton = document.getElementById('stunts-RollHere');
+    const stuntsResult = document.getElementById('stunts-result');
+/* Skills - Brawl */
+    const brawlDie = document.getElementById('brawl-die');
+    const brawlCode = document.getElementById('brawl-code');
+    const brawlRollCode = document.getElementById('brawl-RollCode');
+    const brawlRollHereButton = document.getElementById('brawl-RollHere');
+    const brawlResult = document.getElementById('brawl-result');
+/* Skills - Tough */
+    const toughDie = document.getElementById('tough-die');
+    const toughCode = document.getElementById('tough-code');
+    const toughRollCode = document.getElementById('tough-RollCode');
+    const toughRollHereButton = document.getElementById('tough-RollHere');
+    const toughResult = document.getElementById('tough-result');
+/* Skills - Tech */
+    const techDie = document.getElementById('tech-die');
+    const techCode = document.getElementById('tech-code');
+    const techRollCode = document.getElementById('tech-RollCode');
+    const techRollHereButton = document.getElementById('tech-RollHere');
+    const techResult = document.getElementById('tech-result');
+/* Skills - Weapons */
+    const weaponsDie = document.getElementById('weapons-die');
+    const weaponsCode = document.getElementById('weapons-code');
+    const weaponsRollCode = document.getElementById('weapons-RollCode');
+    const weaponsRollHereButton = document.getElementById('weapons-RollHere');
+    const weaponsResult = document.getElementById('weapons-result');
+/* Skills - Drive */
+    const driveDie = document.getElementById('drive-die');
+    const driveCode = document.getElementById('drive-code');
+    const driveRollCode = document.getElementById('drive-RollCode');
+    const driveRollHereButton = document.getElementById('drive-RollHere');
+    const driveResult = document.getElementById('drive-result');
+/* Skills - Sneak */
+    const sneakDie = document.getElementById('sneak-die');
+    const sneakCode = document.getElementById('sneak-code');
+    const sneakRollCode = document.getElementById('sneak-RollCode');
+    const sneakRollHereButton = document.getElementById('sneak-RollHere');
+    const sneakResult = document.getElementById('sneak-result');
+/* Skills - Wits */
+    const witsDie = document.getElementById('wits-die');
+    const witsCode = document.getElementById('wits-code');
+    const witsRollCode = document.getElementById('wits-RollCode');
+    const witsRollHereButton = document.getElementById('wits-RollHere');
+    const witsResult = document.getElementById('wits-result');
+/* Skills - Hot */
+    const hotDie = document.getElementById('hot-die');
+    const hotCode = document.getElementById('hot-code');
+    const hotRollCode = document.getElementById('hot-RollCode');
+    const hotRollHereButton = document.getElementById('hot-RollHere');
+    const hotResult = document.getElementById('hot-result');
+
+/****************
+EVENT LISTENERS
+****************/
+/* Skills - Stunts */
+    stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+    stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+    stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id));
+/* Skills - Brawl */
+    /* stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+    stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+    stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id)); */
+/* Skills - Tough */
+    /* stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+    stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+    stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id)); */
+/* Skills - Tech */
+    /* stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+    stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+    stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id)); */
+/* Skills - Weapons */
+    /* stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+    stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+    stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id)); */
+/* Skills - Drive */
+    /* stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+    stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+    stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id)); */
+/* Skills - Sneak */
+    /* stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+    stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+    stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id)); */
+/* Skills - Wits */
+    /* stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+    stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+    stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id)); */
+/* Skills - Hot */
+    /* stuntsDie.addEventListener('change', UpdateCopyText.bind("Stunts", stuntsDieValue, "stunts-code"));
+    stuntsRollHereButton.addEventListener('click', RollSkillHere.bind(stuntsDie.value, stuntsResult.id));
+    stuntsRollCode.addEventListener('click', CopyToClipboard.bind(stuntsCode.id)); */
