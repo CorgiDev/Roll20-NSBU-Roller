0f8cf4e3d6f7f40c9129d2bd127784ca872ef3e9
CorgiDev <corgidev@github.com>
2025-03-12 14:36:40 -0400

    Pasted in a function to copy text that works.

--- web interface/scripts/skills.js
+++ web interface/scripts/skills.js
@@ -45,3 +45,27 @@ function Roll20Chat() {
     // Alert the copied text
     alert("Copied the text: " + copyText.value);
 }
+
+function CopyToClipboard(containerid) {
+    if (window.getSelection) {
+        if (window.getSelection().empty) { // Chrome
+            window.getSelection().empty();
+        } else if (window.getSelection().removeAllRanges) { // Firefox
+            window.getSelection().removeAllRanges();
+        }
+    } else if (document.selection) { // IE?
+        document.selection.empty();
+    }
+
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select().createTextRange();
+        document.execCommand("copy");
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+    }
+}
