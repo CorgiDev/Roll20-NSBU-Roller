3070a58c57958bde9f72a68b08fedb6abf12a4d4
CorgiDev <corgidev@github.com>
2025-03-12 10:36:52 -0400

    Renamed some ids and classes for the browser extension version.

--- browser-extension/chrome/scripts/skills.js
+++ browser-extension/chrome/scripts/skills.js
@@ -1,4 +1,11 @@
-function UpdateCopyText(skillName,skillValue, skillCodeDiv) {
+// Rolls a die of provided number of sides
+function RollDice(sides) {
+    const roll = Math.floor(Math.random() * sides) + 1;
+    return roll;
+}
+
+// Updates the Roll20 Text in the code block
+function UpdateCopyText(skillName,skillValue,skillCodeDiv) {
     const skilldiv = document.getElementById(skillCodeDiv)
     if (skillValue ==0){
         skilldiv.textContent = "Skill currently does not have a valid value set.";
@@ -8,14 +15,8 @@ function UpdateCopyText(skillName,skillValue, skillCodeDiv) {
     }
 }
 
-function RollDice(sides) {
-    const roll = Math.floor(Math.random() * sides) + 1;
-    return roll;
-}
-
-function SetSkillValue() {}
-
-function RollSkill(dieSides, skillResultDiv){
+// Rolls the dice in the window.
+function RollSkillHere(dieSides, skillResultDiv){
     if (dieSides == 0) {
         const div = document.getElementById(skillResultDiv)
         div.textContent = "Unable to roll until die value is set.";
@@ -26,15 +27,8 @@ function RollSkill(dieSides, skillResultDiv){
     }
 }
 
-function Roll20Chat(){}
-
-function RollSkillBrowser(){}
-
-function Roll20Chat() {
-    document.querySelector("textarea").innerHTML = text;
- }
-
- function CopyText (paraID) {
+// Allows the text to be copied to the clipboard
+function CopyText (paraID) {
       // Get the text field
     var copyText = document.getElementById(paraID);
     // Select the text field
@@ -44,4 +38,9 @@ function Roll20Chat() {
     navigator.clipboard.writeText(copyText.value);
     // Alert the copied text
     alert("Copied the text: " + copyText.value);
+}
+
+// Sends provided text to the Roll20 chat window and sends the message.
+function Roll20Chat(chatArray) {
+    document.querySelector("textarea").innerHTML = text;
 }
